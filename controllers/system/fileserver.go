4e083068e127053a831a260c4a9e6692513156e3
Carl Turechek <carl@siteraiser.com>
2025-11-18 13:28:01 -0500

    cleanup

--- controllers/system/fileserver.go
+++ controllers/system/fileserver.go
@@ -11,17 +11,13 @@ import (
 func Fs(root http.Dir) http.Handler {
 	fs := http.FileServer(root)
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("root", root)
 		cleanPath := filepath.Clean(r.URL.Path)
-		fmt.Println("trying:", cleanPath)
 		fullPath := filepath.Join(string(root), cleanPath)
-		fmt.Println("string(root) ", string(root))
-		fmt.Println("cleanPath ", cleanPath)
 
-		fmt.Println("fullPath ", fullPath)
+		fmt.Println("Serving file: ", fullPath)
 
 		if _, err := os.Stat(fullPath); os.IsNotExist(err) {
-			fmt.Println("file nto sound", err)
+			fmt.Println("file not found: ", err)
 			http.NotFound(w, r)
 			return
 		}
