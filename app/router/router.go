996bba1d77f469127076418a9a80033398b0872c
Carl Turechek <carl@siteraiser.com>
2025-11-28 14:21:12 -0500

    Update router.go

--- app/router/router.go
+++ app/router/router.go
@@ -99,9 +99,7 @@ func RouteIt(path string, method string) (Route, []string, map[string]string, bo
 
 		//Filter by request type
 		if route.RequestType != "" && !strings.EqualFold(route.RequestType, method) {
-			fmt.Println("excluded by request type")
-			fmt.Println("RequestType", route.RequestType)
-			fmt.Println("method", method)
+			//fmt.Println("Excluded by request type")
 			continue
 		}
 
